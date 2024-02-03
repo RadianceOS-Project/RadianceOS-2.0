756a73afda2f5fda070660579d77186c5eb4cc86
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2024-02-04 00:09:54 +0100

    swiched to cpp and made utils class + loop function, implemeted limine, make files to check

---
+++ src/kernel/src/KernelUtils/KernelUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+void* memcpy(void* dest, const void* src, size_t n);
+void* memset(void* s, int c, size_t n);
+void* memmove(void* dest, const void* src, size_t n);
+int memcmp(const void* s1, const void* s2, size_t n);
+static void hcf(void);
+void printf(char* printchars);
---
+++ src/kernel/src/main.cpp
@@ -0,0 +1,61 @@
+#include <limine/limine.h>
+#include <KernelUtils/KernelUtils.h>
+
+// Set the base revision to 1, this is recommended as this is the latest
+// base revision described by the Limine boot protocol specification.
+// See specification for further info.
+
+LIMINE_BASE_REVISION(1)
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, in C, they should
+// NOT be made "static".
+
+struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+bool Running = true;
+struct limine_framebuffer* framebuffer;
+// The following will be our kernel's entry point.
+// If renaming _start() to something else, make sure to change the
+// linker script accordingly.
+void _start(void) {
+    // Ensure the bootloader actually understands our base revision (see spec).
+    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+        hcf();
+    }
+
+    //prints the test hello message
+    printf("Hello from radiace 2.0");
+
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response == NULL
+     || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    // Fetch the first framebuffer.
+    framebuffer = framebuffer_request.response->framebuffers[0];
+
+    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    for (size_t i = 0; i < 100; i++) {
+        volatile uint32_t *fb_ptr = framebuffer->address;
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+    }
+
+    while (Running)
+    {
+        _loop();
+    }
+    // We're done, just hang...
+    hcf();
+}
+
+void _loop(void)
+{
+
+    
+
+}
