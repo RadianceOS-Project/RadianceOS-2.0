db967e791a2267014ab00e3c0cabdee90109fcb1
MrBisquit <william@wtdawson.info>
2024-02-04 10:16:46 +0000

    .

--- src/kernel/src/KernelUtils/KernelUtils.cpp
+++ src/kernel/src/KernelUtils/KernelUtils.cpp
@@ -1,5 +1,5 @@
 #include <KernelUtils/KernelUtils.h>
-#include <limine/limine.h>>
+#include <limine/limine.h>
 
 void* memcpy(void* dest, const void* src, size_t n) {
     uint8_t* pdest = (uint8_t*)dest;
