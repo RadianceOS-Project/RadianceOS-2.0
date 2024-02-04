467f9a0f42585e4ac049c97747646d89bfcff7a9
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2024-02-04 17:39:36 +0100

    m

--- src/kernel/src/VideoUtils/videoutils.cpp
+++ src/kernel/src/VideoUtils/videoutils.cpp
@@ -1,29 +1,29 @@
 #include <limine/limine.h>
 #include <VideoUtils/videoutils.h>
 
-limine_framebuffer framebuffer;
+limine_framebuffer* framebuffer;
 
-void Setframebuffer(limine_framebuffer framebuff)
+void Setframebuffer(limine_framebuffer* framebuff)
 {
 	framebuffer = framebuff;
 }
 void ClearScreen(unsigned int color)
 {
     // Note: we assume the framebuffer model is RGB with 32-bit pixels.
-    for (uint64_t i = 0; i < framebuffer.width; i++) {
-        volatile uint32_t* fb_ptr = framebuffer.address;
+    for (uint64_t i = 0; i < framebuffer->width; i++) {
+        volatile uint32_t* fb_ptr = framebuffer->address;
         fb_ptr[i * (framebuffer->pitch / 4) + i] = color;
     }
 }
 void DrawFilledRectangle(int x,int y,int widht,int height,unsigned int color)
 {
 
-    volatile uint32_t* fb_ptr = framebuffer.address;
-    for (uint64_t i = x; i < framebuffer.width + x; i++)
+    volatile uint32_t* fb_ptr = framebuffer->address;
+    for (uint64_t i = x; i < framebuffer->width + x; i++)
     {
-        for (uint64_t j = 0; j < framebuffer.height + y; j++)
+        for (uint64_t j = 0; j < framebuffer->height + y; j++)
         {
-            fb_ptr[i+j*framebuffer.width] = color;
+            fb_ptr[i+j*framebuffer->width] = color;
         }
     }
 
@@ -31,8 +31,8 @@ void DrawFilledRectangle(int x,int y,int widht,int height,unsigned int color)
 void DrawPoint(int x, int y, unsigned int color)
 {
 
-    volatile uint32_t* fb_ptr = framebuffer.address;
-    fb_ptr[x + y * framebuffer.width] = color;
+    volatile uint32_t* fb_ptr = framebuffer->address;
+    fb_ptr[x + y * framebuffer->width] = color;
 
 }
 void drawLine(int x1, int y1, int x2, int y2,unsigned int color) 
