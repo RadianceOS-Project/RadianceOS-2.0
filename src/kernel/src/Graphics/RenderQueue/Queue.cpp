897f32c9fab12b09003e363f04d12ea33e618dca
MrBisquit <william@wtdawson.info>
2024-02-06 20:34:48 +0000

    Added a really basic function, doesn't do anything but it's still there.

--- src/kernel/src/Graphics/RenderQueue/Queue.cpp
+++ src/kernel/src/Graphics/RenderQueue/Queue.cpp
@@ -1,2 +1,8 @@
 #include <list>
 
+// I don't know how to do most of this, so I think someone else needs to do it.
+
+// The function called every frame that, if needed, will rerender things.
+void FrameRender(void) {
+
+}
